5ecd54f1ee7b9fe9fee4f0a4a286ab8ba0619166
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-18 01:02:10 +0300

    feat: add props for PokeCard

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,9 +4,14 @@ import PokeCard from './PokeCard';
 const PokeList = () => {
     return (
         <div>
-            <PokeCard />
-            <PokeCard />
-            <PokeCard />
+            <PokeCard 
+                img_url="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+                title="Bulbasaur #001"
+                description="There is a plant seed on its back right from 
+                            the day this Pokémon is born. 
+                            The seed slowly grows larger."
+                tags={['Grass', 'Poison']}
+            />
         </div>
     )
 }
