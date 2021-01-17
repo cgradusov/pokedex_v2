f24e143043e2d9d15014631c85217597034436be
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-17 20:05:10 +0300

    feat: update app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PokeHeader from "./PokeHeader";
+import PokeList from "./PokeList";
 import 'antd/dist/antd.css';
 
 const App = () => {
   return (
-    <PokeHeader />
+    <>
+      <PokeHeader />
+      <PokeList />
+    </>
   );
 }
 
