7ade34dac419d7b5e169aad36c9be94724ab3e7b
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-17 20:04:29 +0300

    feat: add PokeCard component

---
+++ src/components/PokeCard.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Card, Avatar, Tag } from 'antd';
+
+const { Meta } = Card;
+
+const PokeCard = ({loading=false}) => {
+    return (
+        <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+            <Meta
+                avatar={
+                    <Avatar src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" />
+                }
+                title="Bulbasaur #001"
+                description={
+                    <>
+                        <p>There is a plant seed on its back right from 
+                            the day this Pokémon is born. 
+                            The seed slowly grows larger.</p>
+                        <Tag color="green">Grass</Tag>
+                        <Tag color="Purple">Poison</Tag>
+                    </>
+                }
+            />
+            
+        </Card>
+
+    )
+}
+
+export default PokeCard
