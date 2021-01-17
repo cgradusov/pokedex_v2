ffb9903fbc843240193ebf9c3f6e1467492219eb
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-17 20:04:52 +0300

    feat: add PokeList component

---
+++ src/components/PokeList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PokeCard from './PokeCard';
+
+const PokeList = () => {
+    return (
+        <div>
+            <PokeCard />
+            <PokeCard />
+            <PokeCard />
+        </div>
+    )
+}
+
+export default PokeList
