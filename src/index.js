6356f7a96cd1a797afb84d72808b4a3a94120506
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-19 17:38:53 +0300

    feat: connect app to store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-// import { Provider } from 'react-redux';
+import App from './app/App';
+import { Provider } from 'react-redux';
+import store from './app/store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
