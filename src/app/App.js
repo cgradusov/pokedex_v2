5714840dc61d1a91d0bb00afbdb6f31ce6360576
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-19 17:39:56 +0300

    chore: move app component to separate directory

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PokeHeader from "./PokeHeader";
-import PokeList from "./PokeList";
+import PokeHeader from "../components/PokeHeader";
+import PokeList from "../features/pokeList/PokeList";
 import 'antd/dist/antd.css';
 
 const App = () => {
