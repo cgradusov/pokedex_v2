5187ec5f09fbfad880be65d38c94fc2cb7bbe0ce
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-19 17:33:51 +0300

    feat: add string formaters to PokeCard

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -2,17 +2,18 @@
 import React from 'react'
 import { Card, Avatar } from 'antd';
 import PokeTag from './PokeTag';
+import { capitalizeString, formatNumber } from "../utils/stringUtils";
 
 const { Meta } = Card;
 
-const PokeCard = ({loading=false, img_url, title, description, tags}) => {
+const PokeCard = ({loading=false, img_url, name, num, description, tags}) => {
     return (
         <Card loading={loading}>
             <Meta
                 avatar={
                     <Avatar src={img_url} />
                 }
-                title={title}
+                title={capitalizeString(name) + ' #' + formatNumber(num)}
                 description={
                     <>
                         <p>{description}</p>
