5f204c629bcc2abbefa0b6f9c33322691429136e
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-21 14:00:09 +0300

    style: update PokeTag component

--- src/components/PokeTag.js
+++ src/components/PokeTag.js
@@ -1,17 +1,28 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { Tag } from 'antd';
 import { capitalizeString } from "../utils/stringUtils";
-
-const colorsMap = {
-    grass: 'green',
-    poison: 'purple'
-}
+import { getTagStyleMixin } from '../utils/pokeColorUtils';
 
 const PokeTag = ({ type }) => {
+    const tagStyle = {
+        boxSizing: 'border-box',
+        fontSize: '12px',
+        lineHeight: '20px',
+        fontVariant: 'tabular-nums',
+        fontFeatureSettings: 'tnum',
+        display: 'inline-block',
+        height: 'auto',
+        whiteSpace: 'nowrap',
+        margin: '0 8px 0 0',
+        padding: '0 7px',
+
+        ...getTagStyleMixin(type)   
+    }
+
     return (
-        <Tag color={colorsMap[type]}>{capitalizeString(type)}</Tag>
-    )
+            <span style={tagStyle}>{capitalizeString(type)}</span>
+        )
+    
 }
 
 export default PokeTag
