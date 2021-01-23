ecd330ae655661675b8a33275815d67a29fa1433
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-23 16:26:50 +0300

    feat: add Footer component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Layout } from 'antd';
 import PokeHeader from '../components/PokeHeader';
 import PokeList from '../features/pokeList/PokeList';
 import 'antd/dist/antd.css';
+
+const { Footer } = Layout;
 
 const App = () => (
   <>
     <PokeHeader />
     <PokeList />
+    <Footer style={{ textAlign: 'center' }}>
+      Pokedex App ©2021 Created by
+      {' '}
+      <a href="https://github.com/cgradusov">Constantine Gradusov</a>
+    </Footer>
   </>
 );
 
