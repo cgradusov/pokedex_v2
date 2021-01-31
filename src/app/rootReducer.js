afa1d3c4ed78b4f7271869683b0635df422db150
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-01-31 17:36:10 +0300

    refactor: create feature for pagination

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import pokeListReducer from '../features/pokeList/pokeListSlice';
 import pokeSearchReducer from '../features/pokeSearch/pokeSearchSlice';
+import pokePaginationReducer from '../features/pokePagination/pokePaginationSlice';
 
 const rootReducer = combineReducers({
   pokeList: pokeListReducer,
   pokeSearch: pokeSearchReducer,
+  pokePagination: pokePaginationReducer,
 });
 
 export default rootReducer;
---
+++ src/features/pokePagination/PokePagination.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useDispatch, connect } from 'react-redux';
+import { Row, Col, Pagination } from 'antd';
+import { changePageNumber } from './pokePaginationSlice';
+
+const PokePagination = ({
+  pokeList, pageNumber, count, pokemonsPerPage,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Row gutter={[0, 16]} justify="center">
+      <Col style={{ margin: '15px 0' }}>
+        {pokeList.length !== 0
+          ? (
+            <Pagination
+              current={pageNumber}
+              total={count}
+              onChange={(number) => dispatch(changePageNumber(number))}
+              defaultPageSize={pokemonsPerPage}
+              showSizeChanger={false}
+            />
+          )
+          : <h1>Not Found</h1>}
+      </Col>
+    </Row>
+  );
+};
+
+const mapState = (state) => ({
+  pokeList: state.pokeList.pokemonsList,
+  count: state.pokeList.count,
+
+  pageNumber: state.pokePagination.pageNumber,
+  pokemonsPerPage: state.pokePagination.pokemonsPerPage,
+});
+
+export default connect(mapState, null)(PokePagination);
