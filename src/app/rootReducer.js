2ba3eb78659b37e6f6e39c44f855887952a4d0d2
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-02-08 18:01:47 +0300

    feat: add PokeTypesFilter reducer to rootReducer

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { connectRouter } from 'connected-react-router';
 import pokeListReducer from '../features/pokeList/pokeListSlice';
 import pokePaginationReducer from '../features/pokePagination/pokePaginationSlice';
+import pokeTypesFilterReducer from '../features/pokeTypesFilter/pokeTypesFilterSlice';
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
   pokeList: pokeListReducer,
   pokePagination: pokePaginationReducer,
+  pokeFilter: pokeTypesFilterReducer,
 });
 
 export default createRootReducer;
