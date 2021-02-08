71e114e8cc8a1c2a9d6cbee83d8def288ac015f7
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-02-08 17:58:44 +0300

    feat: add get id from url string util function

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,3 +1,5 @@
 export const capitalizeString = (str) => str.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
 
 export const formatNumber = (str, len = 3) => ('0'.repeat(len - str.length) + str);
+
+export const getIdFromUrl = (str) => { const res = /\/([0-9]+)\//g.exec(str); return Number.parseInt(res[1], 10); };
