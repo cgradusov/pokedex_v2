e5e3a2c32a5b06efa54ee414521f28a568db8ea3
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-02-08 17:56:39 +0300

    feat: update PokeTag to be clickable

--- src/components/PokeTag.jsx
+++ src/components/PokeTag.jsx
@@ -1,26 +1,40 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { capitalizeString } from '../utils/stringUtils';
 import getTagStyleMixin from '../utils/pokeColorUtils';
 
-const PokeTag = ({ type }) => {
+const PokeTag = ({ type = 'grass', width = 'inherit' }) => {
+  const [isType, markType] = useState(false);
+
+  const stateStyles = isType ? {} : {
+    color: 'grey',
+    border: '1px solid grey',
+  };
+
   const tagStyle = {
     boxSizing: 'border-box',
     fontSize: '12px',
     lineHeight: '20px',
     fontVariant: 'tabular-nums',
     fontFeatureSettings: 'tnum',
-    display: 'inline-block',
+    display: 'inline-flex',
     height: 'auto',
     whiteSpace: 'nowrap',
     margin: '0 8px 0 0',
     padding: '0 7px',
+    cursor: 'pointer',
+    userSelect: 'none',
+    width,
+    justifyContent: 'center',
 
     ...getTagStyleMixin(type),
+    ...stateStyles,
   };
 
   return (
-    <span style={tagStyle}>{capitalizeString(type)}</span>
+    <span onClick={() => markType(!isType)} style={tagStyle}>{capitalizeString(type)}</span>
   );
 };
 
