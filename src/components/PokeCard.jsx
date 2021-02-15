d3c0b4d2b7636566b163d72d526824dd925b94c0
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-02-15 16:32:20 +0300

    style: improve PokeCard appearance

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,14 +1,13 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import Card from 'antd/lib/card';
-import Avatar from 'antd/lib/avatar';
+import Skeleton from 'antd/lib/skeleton';
 import { useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
 import { useLocation } from 'react-router-dom';
 import PokeTag from './PokeTag';
 import { capitalizeString, formatNumber } from '../utils/stringUtils';
-
-const { Meta } = Card;
 
 const PokeCard = ({
   loading = false, name, num, specs, types,
@@ -27,12 +26,15 @@ const PokeCard = ({
   const cardStyle = {
     height: '100%',
     margin: '15px 10px',
+    padding: loading ? '24px' : '0',
+    border: '1px solid #f0f0f0',
   };
 
   const descriptionStyle = {
     position: 'absolute',
     bottom: '10px',
     right: '10px',
+    padding: '0 24px',
   };
 
   const location = useLocation();
@@ -47,36 +49,43 @@ const PokeCard = ({
     }));
   };
 
-  const metaStyles = {
-    cursor: 'pointer',
-    userSelect: 'none',
-  };
+  const imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatNumber(num)}.png`;
 
   return (
-    <Card loading={loading} style={cardStyle}>
-      <Meta
-        style={metaStyles}
-        avatar={
-          <Avatar src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatNumber(num)}.png`} />
-                }
-        title={`${capitalizeString(name)} #${formatNumber(num)}`}
-        onClick={onClick}
-        description={(
-          <>
-            {/* eslint-disable-next-line camelcase */}
-            <p>{flavor_text}</p>
-          </>
-        )}
-      />
-      <p />
-      <div style={descriptionStyle}>
-        {tags.map((type) => (
-          <PokeTag type={type} key={type} />
-        ))}
-      </div>
+    <div style={cardStyle}>
+      <Skeleton loading={loading} active>
+        <h2
+          style={{
+            background: `no-repeat url(${imgUrl})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            color: 'white',
+            textShadow: '0px 0px 5px #000000',
+            textAlign: 'center',
+            height: '215px',
+            paddingTop: '20px',
+            cursor: 'pointer',
+            userSelect: 'none',
+          }}
+          onClick={onClick}
+        >
+          {`${capitalizeString(name)} #${formatNumber(num)}`}
 
-    </Card>
-
+        </h2>
+        <p
+          style={{ color: 'rgba(0, 0, 0, 0.45)', padding: '24px' }}
+        >
+          {/* eslint-disable-next-line camelcase */}
+          {flavor_text}
+        </p>
+        <p />
+        <div style={descriptionStyle}>
+          {tags.map((type) => (
+            <PokeTag type={type} key={type} />
+          ))}
+        </div>
+      </Skeleton>
+    </div>
   );
 };
 
